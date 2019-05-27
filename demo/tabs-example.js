ded7bad17133bdff607e701a2d4986393f6ea15c
Miguel Angel Alvarez <malvarez@desarrolloweb.com>
2019-05-27 11:34:08 +0200

    Second demo. Using in tabs implementation

---
+++ demo/tabs-example.js
@@ -0,0 +1,37 @@
+import { LitElement, html } from 'lit-element';
+
+export class TabsExample extends LitElement {
+  static get properties() {
+    return {
+      currentPage: { type: String }
+    };
+  }
+
+  constructor() {
+    super();
+    this.currentPage = 'page3';
+  }
+
+  render() {
+    return html`
+    <nav>
+      <button @click="${this.changePage}" data="page1">Page 1</button>
+      <button @click="${this.changePage}" data="page2">Page 2</button>
+      <button @click="${this.changePage}" data="page3">Page 3</button>
+    </nav>
+    <dile-pages selected="${this.currentPage}" attrforselected="name" id="pages">
+    </dile-pages>
+    `;
+  }
+
+  firstUpdated() {
+    this.shadowRoot.getElementById('pages').innerHTML = this.innerHTML;
+    this.shadowRoot.getElementById('pages')._pageInitialization();
+  }
+
+  changePage(e) {
+    console.log(e.target.getAttribute('data')); 
+    this.currentPage = e.target.getAttribute('data');
+  }
+}
+customElements.define('tabs-example', TabsExample);
